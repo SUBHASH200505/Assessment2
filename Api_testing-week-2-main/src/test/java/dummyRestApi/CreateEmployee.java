f45105f193248f4829d9228e3d33a06360f082b9
SUBHASH200505 <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-09 10:36:53 +0530

    Update CreateEmployee.java

--- Api_testing-week-2-main/src/test/java/dummyRestApi/CreateEmployee.java
+++ Api_testing-week-2-main/src/test/java/dummyRestApi/CreateEmployee.java
@@ -7,7 +7,7 @@ public class CreateEmployee {
 	public static void main(String[] args) {
 		RestAssured.given()
 		.contentType("application/json")
-		.body("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}")
+		.body("{\"name\":\"subhash\",\"salary\":\"30000\",\"age\":\"13\"}")
 		
 		.when()
 			.post("https://dummy.restapiexample.com/api/v1/create")
