c5acedb107e4398bd20e0aeb6978f359ca2fc02b
SUBHASH200505 <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-09 10:40:42 +0530

    Update UpdateUserTest.java

--- Api_testing-week-2-main/src/test/java/task2/UpdateUserTest.java
+++ Api_testing-week-2-main/src/test/java/task2/UpdateUserTest.java
@@ -11,8 +11,8 @@ public class UpdateUserTest {
         RestAssured.baseURI = "http://49.249.28.218:8091";
 
         String body = "{\n" +
-                "  \"createdBy\": \"Sabarinathan\",\n" +
-                "  \"projectName\": \"UpdatedProject\",\n" +
+                "  \"createdBy\": \"rockstar\",\n" +
+                "  \"projectName\": \"hello\",\n" +
                 "  \"status\": \"Ongoing\",\n" +
                 "  \"teamSize\": 0\n" +
                 "}";
