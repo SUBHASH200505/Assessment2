b81ea29c1e46ccb71012948c1965c27f67c51424
SUBHASH200505 <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-09 10:37:23 +0530

    Update UpdateByID.java

--- Api_testing-week-2-main/src/test/java/dummyRestApi/UpdateByID.java
+++ Api_testing-week-2-main/src/test/java/dummyRestApi/UpdateByID.java
@@ -7,7 +7,7 @@ public class UpdateByID {
 	public static void main(String[] args) {
 		RestAssured.given()
 		.contentType("application/json")
-		.body("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}")
+		.body("{\"name\":\"rock\",\"salary\":\"20000\",\"age\":\"10\"}")
 		
 		.when()
 			.put("https://dummy.restapiexample.com/api/v1/update/7")
